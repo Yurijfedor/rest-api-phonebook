b8d80ee7822dfe0795c7af4739b271c1fe6da79f
Yurii Shaklak <99280782+Yurijfedor@users.noreply.github.com>
2023-01-25 12:57:27 +0200

    add tokenType

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,9 +2,12 @@ const jwt = require('jsonwebtoken')
 const {NotAuthorizedError} = require('../helpers/errors')
 
 const authMiddleware = (reg, res, next) => {
-    const [_, token] = reg.headers['authorization'].split(' ')
+    const [tokenType, token] = reg.headers['authorization'].split(' ')
     if (!token) {
         next(new NotAuthorizedError('Please, provide a token'))
+    }
+    if (tokenType !== 'Bearer') {
+        next(new NotAuthorizedError('Please provide the correct token type'))
     }
     try {
         const user = jwt.decode(token, process.env.JWT_SECRET)
